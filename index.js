2bb600a8eb01160af8d14de11ac4721e1ddbe3d2
shajid444 <mdshajidhasan76@gmail.com>
2024-06-12 11:49:35 +0600

    after implement delete operation of cart section

--- index.js
+++ index.js
@@ -64,6 +64,16 @@ async function run() {
     })
 
 
+    app.delete('/cart/:id', async (req, res) => {
+
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+
+    })
+
+
 
 
 
